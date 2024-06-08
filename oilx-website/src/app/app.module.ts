ff41ab42e592f8d57381587af7248127a3b8a15c
Akpodigha Filatei <filatei@gtsng.com>
2024-06-08 17:00:47 +0100

    oilx site main draft

--- oilx-website/src/app/app.module.ts
+++ oilx-website/src/app/app.module.ts
@@ -3,7 +3,7 @@ import { RoadmapComponent } from './components/roadmap/roadmap.component';
 import { OilconomicsComponent } from './components/oilconomics/oilconomics.component';
 import { FaqComponent } from './components/faq/faq.component';
 import { BenefitsComponent } from './components/benefits/benefits.component';
-import { NgModule } from '@angular/core';
+import { NgModule, isDevMode } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
 
@@ -11,11 +11,17 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { ServiceWorkerModule } from '@angular/service-worker';
 const customComponents = [HomeComponent, BenefitsComponent, FaqComponent, OilconomicsComponent, RoadmapComponent];
 
 @NgModule({
   declarations: [AppComponent, ...customComponents],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, ServiceWorkerModule.register('ngsw-worker.js', {
+  enabled: !isDevMode(),
+  // Register the ServiceWorker as soon as the application is stable
+  // or after 30 seconds (whichever comes first).
+  registrationStrategy: 'registerWhenStable:30000'
+})],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
   exports: [...customComponents],
